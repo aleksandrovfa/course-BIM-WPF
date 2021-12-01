ae4e08aeb99ec18a96aee07a7cdf8d944ada991b
Федор Александров <fedfed95@yandex.ru>
2021-12-02 00:38:26 +0300

    3 занятие

---
+++ 03_Task/01_Part/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_Part
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textbox!= null)
+            {
+                textbox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            int size = Convert.ToInt32(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textbox != null)
+            {
+                textbox.FontSize = size;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == bold)
+            {
+                if (textbox.FontWeight == FontWeights.Normal)
+                {
+                    textbox.FontWeight = FontWeights.Bold;
+                }
+                else
+                {
+                    textbox.FontWeight = FontWeights.Normal;
+                }
+            }
+            else if (e.OriginalSource == italic)
+            {
+                if (textbox.FontStyle == FontStyles.Normal)
+                {
+                    textbox.FontStyle = FontStyles.Italic;
+                }
+                else
+                {
+                    textbox.FontStyle = FontStyles.Normal;
+                }
+            }
+            else if (e.OriginalSource == underline)
+            {
+                if (textbox.TextDecorations == null)
+                {
+                    textbox.TextDecorations = TextDecorations.Underline;
+                }
+                else
+                {
+                    textbox.TextDecorations = null;
+                }
+            }
+        }
+    }
+}
