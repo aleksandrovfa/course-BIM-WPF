59df8854efa7e9d30f396e0d3dd5b2627efb339e
Fedor_Aleksandrov <aleksandrov.fa95@gmail.com>
2021-12-07 23:56:34 +0300

    7 Занятие

---
+++ 07_Task/01_Part/WindowCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _01_Part
+{
+    public class WindowCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+        static WindowCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            Exit = new RoutedCommand("Exit", typeof(MainWindow));
+        }
+
+    }
+}
