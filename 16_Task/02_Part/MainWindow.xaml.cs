a2baca6605c551f89668c6995e89b5183826d42a
Федор Александров <fedfed95@yandex.ru>
2021-12-19 17:39:56 +0300

    16 Задание 2

---
+++ 16_Task/02_Part/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_Part
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ellipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.To = Canvas.GetTop(ell) - 700;
+            doubleAnimation.Duration = TimeSpan.FromSeconds(1.5);
+            doubleAnimation.AutoReverse = true;
+            doubleAnimation.DecelerationRatio = 1;
+            ell.BeginAnimation(TopProperty, doubleAnimation);
+        }
+    }
+}
