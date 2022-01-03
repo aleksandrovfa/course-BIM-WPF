c9a9016e4561b932b02295bce31cc9d03834c080
Федор Александров <fedfed95@yandex.ru>
2022-01-04 02:14:47 +0300

    19 задание

---
+++ 19_Task/01_Part/Models/Ariph.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace _01_Part
+{
+    static class Ariph
+    {
+        public static double Add(double a) => 2*Math.PI*a;
+    }
+}
---
+++ 19_Task/01_Part/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _01_Part.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Radius = Ariph.Add(Length);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Radius != 0 || Length != 0)
+                return true;
+            else
+                return
+                    false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+
+}
